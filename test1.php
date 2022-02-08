3771e91d5e0e70ffbf479757212739df7e781ab1
testt <foffd@naver.com>
2022-02-08 17:15:35 +0900

    test commit

---
+++ test1.php
@@ -0,0 +1,18 @@
+<?
+class Test extends Hello {
+	function __construct() {
+	}
+
+	function hihi() {
+		return true;
+	}
+
+	function hoho() {
+		if($this->test) {
+			foreach($arr as $key => $val) {
+				$res = '13';
+			}
+		}
+	}
+}
+?>
